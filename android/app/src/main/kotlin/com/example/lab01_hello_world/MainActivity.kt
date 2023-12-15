965d8219d8900d8616589794953cce725630065c
vuong2002 <phamvuong3322@gmail.com>
2023-12-15 18:34:52 +0700

    project

---
+++ android/app/src/main/kotlin/com/example/lab01_hello_world/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab01_hello_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, World!. Phạm Ngọc Vượng',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
